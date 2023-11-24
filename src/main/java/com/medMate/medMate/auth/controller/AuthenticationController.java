774787696172be229f7d0f7af50caf6a5babeee7
Duru Oluchi M <duwend46@gmail.com>
2023-11-24 12:45:23 +0100

    activating production enviroment

--- src/main/java/com/medMate/medMate/auth/controller/AuthenticationController.java
+++ src/main/java/com/medMate/medMate/auth/controller/AuthenticationController.java
@@ -1,64 +1,64 @@
-//package com.medMate.medMate.auth.controller;
-//
-//import com.medMate.medMate.auth.dto.request.AuthenticationRequest;
-//import com.medMate.medMate.auth.dto.response.AuthResponse;
-//import com.medMate.medMate.auth.dto.response.AuthenticationResponse;
-//import com.medMate.medMate.auth.dto.response.BaseResponse;
-//import com.medMate.medMate.auth.exception.AlreadyExistException;
-//import com.medMate.medMate.auth.service.AuthenticationService;
-//import com.medMate.medMate.user.data.models.User;
-//import com.medMate.medMate.medications.medicationexception.NotFoundException;
-//import com.medMate.medMate.user.dto.request.RegisterRequest;
-//import com.medMate.medMate.user.dto.response.RegisterUserResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.time.LocalDateTime;
-//
-//@RestController
-//@RequestMapping("/api/v1/auth")
-//@RequiredArgsConstructor
-//public class AuthenticationController {
-//    private final AuthenticationService service;
-//
-//    @PostMapping("/register")
-//    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
-//        try {
-//            User user = service.register(request);
-//            return new ResponseEntity<>(
-//                    new RegisterUserResponse(true, HttpStatus.CREATED.value(), "successful",
-//                            LocalDateTime.now(), user)
-//                    ,HttpStatus.CREATED);
-//        }
-//        catch (AlreadyExistException e){
-//            return new ResponseEntity<>(
-//                    new BaseResponse(false, HttpStatus.BAD_REQUEST.value(), e.getMessage(),
-//                            LocalDateTime.now())
-//                    ,HttpStatus.BAD_REQUEST);
-//        }
-//
-//    }
-//
-//    @PostMapping("/authenticate")
-//    public ResponseEntity<?> login(@RequestBody AuthenticationRequest request) {
-//        try {
-//            AuthenticationResponse response = service.authenticate(request);
-//            return new ResponseEntity<>(
-//                    new AuthResponse(true, HttpStatus.OK.value(), "successful",
-//                            LocalDateTime.now(), response)
-//                    ,HttpStatus.OK);
-//        }
-//        catch (NotFoundException e){
-//            return new ResponseEntity<>(
-//                    new BaseResponse(false, HttpStatus.BAD_REQUEST.value(), e.getMessage(),
-//                            LocalDateTime.now())
-//                    ,HttpStatus.BAD_REQUEST);
-//        }
-//
-//    }
-//}
+package com.medMate.medMate.auth.controller;
+
+import com.medMate.medMate.auth.dto.request.AuthenticationRequest;
+import com.medMate.medMate.auth.dto.response.AuthResponse;
+import com.medMate.medMate.auth.dto.response.AuthenticationResponse;
+import com.medMate.medMate.auth.dto.response.BaseResponse;
+import com.medMate.medMate.auth.exception.AlreadyExistException;
+import com.medMate.medMate.auth.service.AuthenticationService;
+import com.medMate.medMate.user.data.models.User;
+import com.medMate.medMate.medications.medicationexception.NotFoundException;
+import com.medMate.medMate.user.dto.request.RegisterRequest;
+import com.medMate.medMate.user.dto.response.RegisterUserResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            User user = service.register(request);
+            return new ResponseEntity<>(
+                    new RegisterUserResponse(true, HttpStatus.CREATED.value(), "successful",
+                            LocalDateTime.now(), user)
+                    ,HttpStatus.CREATED);
+        }
+        catch (AlreadyExistException e){
+            return new ResponseEntity<>(
+                    new BaseResponse(false, HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+                            LocalDateTime.now())
+                    ,HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> login(@RequestBody AuthenticationRequest request) {
+        try {
+            AuthenticationResponse response = service.authenticate(request);
+            return new ResponseEntity<>(
+                    new AuthResponse(true, HttpStatus.OK.value(), "successful",
+                            LocalDateTime.now(), response)
+                    ,HttpStatus.OK);
+        }
+        catch (NotFoundException e){
+            return new ResponseEntity<>(
+                    new BaseResponse(false, HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+                            LocalDateTime.now())
+                    ,HttpStatus.BAD_REQUEST);
+        }
+
+    }
+}
--- src/main/java/com/medMate/medMate/auth/service/AuthenticationService.java
+++ src/main/java/com/medMate/medMate/auth/service/AuthenticationService.java
@@ -1,75 +1,75 @@
-//package com.medMate.medMate.auth.service;
-//
-//import com.medMate.medMate.auth.dto.request.AuthenticationRequest;
-//import com.medMate.medMate.auth.dto.response.AuthenticationResponse;
-//import com.medMate.medMate.auth.exception.AlreadyExistException;
-//import com.medMate.medMate.medications.medicationexception.NotFoundException;
-//import com.medMate.medMate.security.config.JwtService;
-//import com.medMate.medMate.user.data.models.User;
-//import com.medMate.medMate.auth.data.repositories.UserRepository;
-//import com.medMate.medMate.user.dto.request.RegisterRequest;
-//import com.medMate.medMate.user.enums.Role;
-//import com.medMate.medMate.user.services.PatientProfileService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Optional;
-//
-//import static com.medMate.medMate.medications.medicationexception.ExceptionMessage.EMAIL_ALREADY_EXIST;
-//import static com.medMate.medMate.medications.medicationexception.ExceptionMessage.EMAIL_OR_PASSWORD_IS_NOT_CORRECT;
-//
-//@Service
-//@RequiredArgsConstructor
+package com.medMate.medMate.auth.service;
+
+import com.medMate.medMate.auth.dto.request.AuthenticationRequest;
+import com.medMate.medMate.auth.dto.response.AuthenticationResponse;
+import com.medMate.medMate.auth.exception.AlreadyExistException;
+import com.medMate.medMate.medications.medicationexception.NotFoundException;
+import com.medMate.medMate.security.config.JwtService;
+import com.medMate.medMate.user.data.models.User;
+import com.medMate.medMate.auth.data.repositories.UserRepository;
+import com.medMate.medMate.user.dto.request.RegisterRequest;
+import com.medMate.medMate.user.enums.Role;
+import com.medMate.medMate.user.services.PatientProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.medMate.medMate.medications.medicationexception.ExceptionMessage.EMAIL_ALREADY_EXIST;
+import static com.medMate.medMate.medications.medicationexception.ExceptionMessage.EMAIL_OR_PASSWORD_IS_NOT_CORRECT;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
-//    private  final PasswordEncoder passwordEncoder;
-//    private final UserRepository userRepository;
-//    private final JwtService jwtService;
-//    private final AuthenticationManager authenticationManager;
-//
-//    private final PatientProfileService patientProfileService;
-//    public User register(RegisterRequest request){
-//        Optional<User> founderUser = userRepository.findByEmail(request.getEmail());
-//        if (founderUser.isPresent()){
-//            throw new AlreadyExistException(EMAIL_ALREADY_EXIST.name());
-//        }
-//        else {
-//            var user = User.builder()
-////                    .firstName(request.getFirstname())
-////                    .lastName(request.getLastname())
-//                    .password(passwordEncoder.encode(request.getPassword()))
-//                    .email(request.getEmail())
-//                    .role(Role.PATIENT)
-//                    .build();
-//            userRepository.save(user);
-//            patientProfileService.createPatientProfile(user);
-//            return user;
-//        }
-//
-////    var jwtToken = jwtService.generateToken(user);
-////        return AuthenticationResponse.builder()
-////                .token(jwtToken)
-////                .build();
-//    }
-//
-//    public AuthenticationResponse authenticate(AuthenticationRequest request) {
-//        Optional<User> user = userRepository.findByEmail(request.getEmail());
-//
-//        if (user.isEmpty()){
-//            throw new NotFoundException(EMAIL_OR_PASSWORD_IS_NOT_CORRECT.name());
-//        }  else {
-//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-//                    request.getEmail(),
-//                    request.getPassword()
-//            ));
-//            var jwtToken = jwtService.generateToken((UserDetails) user.get());
-//            return AuthenticationResponse.builder()
-//                    .token(jwtToken)
-//                    .build();
-//        }
-//
-//    }
+    private  final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    private final PatientProfileService patientProfileService;
+    public User register(RegisterRequest request){
+        Optional<User> founderUser = userRepository.findByEmail(request.getEmail());
+        if (founderUser.isPresent()){
+            throw new AlreadyExistException(EMAIL_ALREADY_EXIST.name());
+        }
+        else {
+            var user = User.builder()
+//                    .firstName(request.getFirstname())
+//                    .lastName(request.getLastname())
+                    .password(passwordEncoder.encode(request.getPassword()))
+                    .email(request.getEmail())
+                    .role(Role.PATIENT)
+                    .build();
+            userRepository.save(user);
+            patientProfileService.createPatientProfile(user);
+            return user;
+        }
+
+//    var jwtToken = jwtService.generateToken(user);
+//        return AuthenticationResponse.builder()
+//                .token(jwtToken)
+//                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        Optional<User> user = userRepository.findByEmail(request.getEmail());
+
+        if (user.isEmpty()){
+            throw new NotFoundException(EMAIL_OR_PASSWORD_IS_NOT_CORRECT.name());
+        }  else {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    request.getEmail(),
+                    request.getPassword()
+            ));
+            var jwtToken = jwtService.generateToken((UserDetails) user.get());
+            return AuthenticationResponse.builder()
+                    .token(jwtToken)
+                    .build();
+        }
+
+    }
 }
