cfa4221773f51bc3bd5cd0fefb6dd8023308204a
Duru Oluchi M <duwend46@gmail.com>
2023-11-25 23:35:46 +0100

    update

--- src/main/java/com/medMate/medMate/MedMateApplication.java
+++ src/main/java/com/medMate/medMate/MedMateApplication.java
@@ -3,8 +3,13 @@ package com.medMate.medMate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MedMateApplication {
@@ -14,14 +19,19 @@ public class MedMateApplication {
 	}
 
 	@Bean
-	public  WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer () {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("https://medmatebackend2-production.up.railway.app/*")
-						.allowedHeaders("*");
-			}
-		};
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://medmatebackend2-production.up.railway.app/", "https://medmatebackend2-production.up.railway.app/**",  "http://localhost:3001/**",   "http://localhost:3000/**","http://localhost:3000/" ,"http://localhost:3000"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 
 }
