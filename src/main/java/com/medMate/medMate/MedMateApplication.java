f91f5b329d746bf8f4b9c6fd2043603ac654f870
Duru Oluchi M <duwend46@gmail.com>
2023-11-25 15:43:02 +0100

    update cross

--- src/main/java/com/medMate/medMate/MedMateApplication.java
+++ src/main/java/com/medMate/medMate/MedMateApplication.java
@@ -2,12 +2,26 @@ package com.medMate.medMate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MedMateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedMateApplication.class, args);
+	}
+
+	@Bean
+	public  WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer () {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
